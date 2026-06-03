5e460ae8f1598affc901ced3e9efe20cb8760b50
Zixaun <zixuany228@gmail>
2026-06-03 11:46:22 +0630

    Enable SSL verification for FCM notifications

--- app/Helpers/notification_helper.php
+++ app/Helpers/notification_helper.php
@@ -149,11 +149,7 @@ function sendFcmNotification($url, $access_token, $data)
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
     curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-
-    // Disabling SSL Certificate support temporarly
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
 
     // Execute post
