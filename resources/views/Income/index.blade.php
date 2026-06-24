8462c93ae7b6ca1551e05c25fe4cc3f063d62dc8
Zixaun <zixuany228@gmail>
2026-06-24 15:36:02 +0630

    Show success toast after creating fees

--- resources/views/Income/index.blade.php
+++ resources/views/Income/index.blade.php
@@ -571,7 +571,8 @@
             return dueChargesAmount;
         }
 
-        function successFunction() {
+        function successFunction(response) {
+            showSuccessToast(response.message);
             $('.compulsory-fees-types [data-repeater-item]').slice(1).empty();
             $('.fees-installment-repeater [data-repeater-item]').slice(0).empty();
             $('.fees-installment-repeater').hide();
