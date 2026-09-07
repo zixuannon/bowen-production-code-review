0b8780c1a50a9e7f5a32afdf08e213c35b57364e
Zixaun <zixuany228@gmail>
2026-09-07 11:03:52 +0630

    Fix onboarding contract test path

--- tests/Unit/FrontDeskOnboardingContractTest.php
+++ tests/Unit/FrontDeskOnboardingContractTest.php
@@ -8,13 +8,14 @@ class FrontDeskOnboardingContractTest extends TestCase
 {
     public function test_front_desk_role_is_exposed_without_implicit_finance_grants(): void
     {
-        $controller = file_get_contents(base_path('app/Http/Controllers/StaffController.php'));
-        $view = file_get_contents(base_path('resources/views/staff/index.blade.php'));
-        $command = file_get_contents(base_path('app/Console/Commands/ProvisionFrontDeskRole.php'));
+        $root = dirname(__DIR__, 2);
+        $controller = file_get_contents($root . '/app/Http/Controllers/StaffController.php');
+        $view = file_get_contents($root . '/resources/views/staff/index.blade.php');
+        $command = file_get_contents($root . '/app/Console/Commands/ProvisionFrontDeskRole.php');
 
         self::assertStringContainsString('Front Desk / Admissions & Collection', $controller);
         self::assertStringContainsString("name=\"role_ids[]\"", $view);
-        self::assertStringContainsString("submit_collections", file_get_contents(base_path('app/Http/Controllers/FinanceGroupController.php')));
+        self::assertStringContainsString("submit_collections", file_get_contents($root . '/app/Http/Controllers/FinanceGroupController.php'));
         self::assertStringContainsString('school:provision-front-desk-role {school_code}', $command);
         self::assertStringNotContainsString('operate_finance', $command);
     }
